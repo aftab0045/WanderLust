ec7b9a3f52d7b36c639b783e12e10b7aa245a98d
aftab0045 <aftabattar0045@gmail.com>
2025-08-23 00:52:40 +0530

    add

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
 
+// Signup Route
 router.post(
   "/signup",
   wrapAsync(async (req, res) => {
@@ -23,5 +25,15 @@ router.post(
     }
   })
 );
+
+// Login Route
+router.get("/login", (req,res) =>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local",{ failureRedirect: "/login" , failureFlash: true }) , async(req,res) =>{
+    req.flash("success","Welcome to WanderLost! Logged In Successfull !");
+    res.redirect("/listings");
+});
 
 module.exports = router;
