7b418765e5d2725e50968cec2918228c04f8ebb0
aftab0045 <aftabattar0045@gmail.com>
2025-08-23 13:15:12 +0530

    add

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
 const ExpressError = require("./utils/ExpressError");
 const {listingSchema,reviewSchema} = require("./schema.js");
 
@@ -49,4 +50,14 @@ module.exports.validateReview = (req,res,next) =>{
     else{
         next();
     }
+};
+
+module.exports.isReviewAuthor = async (req,res,next) => {
+    let {id,reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error", "You're Not Author Of This Review!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams:true});
 const wrapAsync = require("../utils/wrapAsync");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing");
-const {validateReview, isLoggedIn} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 
 
@@ -23,7 +23,7 @@ router.post("/", isLoggedIn , validateReview, wrapAsync(async(req,res) =>{
 }));
 
 // Delete Review Route
-router.delete("/:reviewId", wrapAsync(async(req,res) =>{
+router.delete("/:reviewId", isLoggedIn , isReviewAuthor ,wrapAsync(async(req,res) =>{
     let{id,reviewId} = req.params;
     await Listing.findByIdAndUpdate(id, {$pull: {review: reviewId}});
     await Review.findByIdAndDelete(reviewId);
